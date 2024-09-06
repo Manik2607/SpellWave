ad498a2ff060288a7f5f9da57b35963bf9cc7bfe
= <=>
2024-09-06 10:56:20 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from './components/ui/button';
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
+export default App; 
+
